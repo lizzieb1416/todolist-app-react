a3107d5039ebc54bf69ed3a97b1673e13bcbdf72
lizzieb1416 <lisetbastidasg@gmail.com>
2022-01-25 22:19:15 +0100

    forgot to save

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,7 +5,7 @@ function TodoList({ todos, setTodos, filteredTodos }) {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {filteredTodos.map((todo) => {
+        {todos.map((todo) => {
           return (
             <Todo
               key={todo.id}
